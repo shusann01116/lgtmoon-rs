97f75b90f8d06a4d700a2c04a7329413504fc2c5
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2025-02-19 21:56:34 +0900

    Remove webpack's setting

--- web-app/next.config.ts
+++ web-app/next.config.ts
@@ -7,16 +7,6 @@ const nextConfig: NextConfig = {
   },
   trailingSlash: false,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
-  webpack: (config, { isServer, dev }) => {
-    config.experiments = {
-      ...config.experiments,
-      asyncWebAssembly: true,
-      layers: true,
-    };
-    config.output.webassemblyModuleFilename = (isServer && !dev ? "../" : "") + "static/wasm/[modulehash].wasm";
-    config.infrastructureLogging = { debug: /PackFileCache/ }
-    return config;
-  }
 };
 
 export default nextConfig;
