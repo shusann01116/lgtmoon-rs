866a13fce4bcab3a10e20dd2eeacdeb64a41f73a
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2025-02-20 20:50:10 +0900

    Add cp and Github Link

--- web-app/src/app/page.tsx
+++ web-app/src/app/page.tsx
@@ -1,11 +1,22 @@
 import { ImageForm } from "@/components/ImageForm";
+import { ExternalLink } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
 	return (
-		<div className="pt-4 max-w-screen-lg mx-auto">
-			<main>
+		<div className="flex flex-col min-h-screen pb-4">
+			<main className="w-full max-w-screen-md pt-4 px-4 mx-auto flex-1">
 				<ImageForm />
 			</main>
+			<footer className="text-center text-sm text-muted-foreground">
+				<p>© 2025 shusann01116</p>
+				<p>
+					<Link href="https://github.com/shusann01116/LGTMoon-rs">
+						GitHub
+						<ExternalLink className="size-4 inline-block pb-1 pl-1" />
+					</Link>
+				</p>
+			</footer>
 		</div>
 	);
 }
