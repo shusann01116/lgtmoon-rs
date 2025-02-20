9b652ed7275131e54de4423e557377b0a45c8a81
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2025-02-20 20:54:16 +0900

    Add feedback for copy button

--- web-app/src/components/ImageCover.tsx
+++ web-app/src/components/ImageCover.tsx
@@ -1,18 +1,45 @@
 import { Button } from "@/components/ui/button";
-import { Clipboard } from "lucide-react";
+import { Check, Clipboard } from "lucide-react";
+import { useEffect, useRef, useState } from "react";
 
 type ImageCoverProps = {
   children: React.ReactNode;
   onClickCopy?: () => void;
-}
+};
 
 export function ImageCover({ children, onClickCopy }: ImageCoverProps) {
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const [isCopied, setIsCopied] = useState(false);
+
+  useEffect(() => {
+    if (!buttonRef.current) return;
+    buttonRef.current.addEventListener("pointerleave", () => {
+      setIsCopied(false);
+    });
+  }, []);
+
+  const onClickCopyInner = () => {
+    if (!onClickCopy) return;
+    onClickCopy();
+    setIsCopied(true);
+  };
+
   return (
     <div className="relative">
       <div className="group absolute inset-0 hover:bg-primary/50 transition-all rounded-sm">
         <div className="flex items-center justify-center h-full">
-          <Button className="group-hover:inline-flex hover:bg-accent/50 hidden transition-all" size="icon" variant="ghost" onClick={onClickCopy}>
-            <Clipboard className="stroke-primary-foreground" />
+          <Button
+            ref={buttonRef}
+            className="group-hover:inline-flex hover:bg-accent/50 hidden transition-all active:bg-accent"
+            size="icon"
+            variant="ghost"
+            onClick={onClickCopyInner}
+          >
+            {isCopied ? (
+              <Check className="stroke-primary-foreground" />
+            ) : (
+              <Clipboard className="stroke-primary-foreground" />
+            )}
           </Button>
         </div>
       </div>
--- web-app/src/components/ImageForm.tsx
+++ web-app/src/components/ImageForm.tsx
@@ -1,11 +1,11 @@
 "use client";
 
-import { ChangeEvent, useRef, useState } from "react";
-import { useLgtmoon } from "@/hooks/useLgtmoon";
+import { ImageCover } from "@/components/ImageCover";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { ImageCover } from "@/components/ImageCover";
+import { useLgtmoon } from "@/hooks/useLgtmoon";
 import { cn } from "@/utils/cn";
+import { type ChangeEvent, useRef, useState } from "react";
 
 export function ImageForm() {
 	const imgRef = useRef<HTMLImageElement>(null);
@@ -19,19 +19,25 @@ export function ImageForm() {
 		} catch (error) {
 			console.error(error);
 		}
-	}
+	};
 
 	const onClickCopy = async () => {
 		if (!imgRef.current) return;
 
-		const buff = await fetch(imgRef.current.src).then((res) => res.arrayBuffer());
-		await navigator.clipboard.write([new ClipboardItem({ "image/png": new Blob([buff], { type: "image/png" }) })]);
-	}
+		const buff = await fetch(imgRef.current.src).then((res) =>
+			res.arrayBuffer(),
+		);
+		await navigator.clipboard.write([
+			new ClipboardItem({
+				"image/png": new Blob([buff], { type: "image/png" }),
+			}),
+		]);
+	};
 
 	return (
-		<div className="flex flex-col gap-4 max-w-md mx-auto">
+		<div className="flex flex-col gap-4">
 			<section className="flex flex-col gap-2">
-				<Label htmlFor="image-input">Image</Label>
+				<Label htmlFor="image-input">LGTMoon</Label>
 				<Input
 					id="image-input"
 					type="file"
@@ -42,7 +48,11 @@ export function ImageForm() {
 			</section>
 			<ImageCover onClickCopy={onClickCopy}>
 				{/* eslint-disable-next-line @next/next/no-img-element */}
-				<img ref={imgRef} className={cn("rounded-sm w-full", show ? "block" : "hidden")} alt="Selected image preview" />
+				<img
+					ref={imgRef}
+					className={cn("rounded-sm w-full", show ? "block" : "hidden")}
+					alt="Selected image preview"
+				/>
 			</ImageCover>
 		</div>
 	);
