7ab2b81a6833ccd7146d7bb4fc1cfa62f2bebaca
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2025-02-22 15:18:12 +0900

    Add delete and download menu for SP

--- web-app/src/features/lgtmoon/ImageCover.tsx
+++ web-app/src/features/lgtmoon/ImageCover.tsx
@@ -1,7 +1,15 @@
 import { Button } from "@/components/ui/button";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { useKeepUntilOnPointerLeave } from "@/hooks/useKeepUntilOnPointerLeave";
 import { cn } from "@/utils/cn";
-import { Check, Clipboard, Download, Trash } from "lucide-react";
+import { PopoverClose } from "@radix-ui/react-popover";
+import {
+	Check,
+	CircleEllipsis,
+	Clipboard,
+	Download,
+	Trash,
+} from "lucide-react";
 import { useRef } from "react";
 import { toast } from "sonner";
 
@@ -38,7 +46,7 @@ export function ImageCover({
 	};
 
 	return (
-		<div className={cn("relative", className)}>
+		<article className={cn("relative", className)}>
 			<div className="group absolute inset-0 hover:bg-primary/50 transition-all rounded-sm">
 				<div className="flex items-center justify-center h-full">
 					<Button
@@ -71,7 +79,7 @@ export function ImageCover({
 						className="group-hover:inline-flex hover:bg-accent/50 hidden transition-all active:bg-accent"
 						size="icon"
 						variant="ghost"
-						onClick={() => onDelete?.()}
+						onClick={onDelete}
 					>
 						<Trash className="stroke-primary-foreground" />
 					</Button>
@@ -83,7 +91,50 @@ export function ImageCover({
 				onClick={onClickCopyForUnHoverable}
 				title="Copy"
 			/>
+			<Popover>
+				<PopoverTrigger asChild>
+					<Button
+						asChild
+						variant="ghost"
+						size="icon"
+						className="absolute stroke-primary-foreground/80 size-11 p-3 right-0 bottom-0 [@media(any-hover:hover)]:hidden"
+					>
+						<CircleEllipsis />
+					</Button>
+				</PopoverTrigger>
+				<PopoverContent
+					side="bottom"
+					align="end"
+					className="w-full p-2 flex flex-col items-start"
+				>
+					<PopoverClose asChild>
+						<Button
+							variant="ghost"
+							size="sm"
+							onClick={onClickDownloadButton}
+						>
+							<Download
+								className="size-4"
+							/>
+							<p className="text-sm">Download</p>
+						</Button>
+					</PopoverClose>
+					<PopoverClose asChild>
+						<Button
+							variant="ghost"
+							size="sm"
+							onClick={onDelete}
+						>
+							<Trash
+								className="size-4"
+								color="hsl(var(--destructive))"
+							/>
+							<p className="text-sm text-destructive">Delete</p>
+						</Button>
+					</PopoverClose>
+				</PopoverContent>
+			</Popover>
 			{children}
-		</div>
+		</article>
 	);
 }
