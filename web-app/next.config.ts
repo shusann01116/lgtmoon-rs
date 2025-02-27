6938cbae7b8d697e416cb0d2a5a468b4b6cf627d
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2025-02-28 08:22:02 +0900

    Remove deploy workflow

--- web-app/next.config.ts
+++ web-app/next.config.ts
@@ -1,11 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	output: "export",
-	images: {
-		unoptimized: true,
-	},
-	trailingSlash: false,
 	basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
 };
 
