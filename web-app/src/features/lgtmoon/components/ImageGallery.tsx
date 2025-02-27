4f00196d0cb6fc87b856cc2cd1c35b386e9e0ffc
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2025-02-28 08:18:56 +0900

    Adjust styling

--- web-app/src/features/lgtmoon/components/ImageGallery.tsx
+++ web-app/src/features/lgtmoon/components/ImageGallery.tsx
@@ -8,7 +8,7 @@ interface ImageGalleryProps {
 
 export const ImageGallery = ({ images, onDelete }: ImageGalleryProps) => {
 	return (
-		<section className="columns-3xs space-y-4">
+		<section className="-mb-4 columns-3xs gap-4 *:mb-4">
 			{images.map((image) => (
 				<LGTMImage key={image.id} image={image} onDelete={onDelete} />
 			))}
