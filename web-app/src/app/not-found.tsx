32f13a4104a8b62456be496e1a3a1c3a0145a670
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2025-03-08 15:15:13 +0900

    Add not found page

---
+++ web-app/src/app/not-found.tsx
@@ -0,0 +1,16 @@
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+
+export default function NotFound() {
+	return (
+		<div className="flex h-screen flex-col items-center justify-center">
+			<h1 className="font-bold text-4xl">404 - Not Found</h1>
+			<p className="text-lg">
+				Sorry, the page you are looking for does not exist.
+			</p>
+			<Link href="/" className="mt-4">
+				<Button className="cursor-pointer">Go to Home</Button>
+			</Link>
+		</div>
+	)
+}
