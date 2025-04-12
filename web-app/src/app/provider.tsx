70ac6b11a950a8c7ccc3f025309f6c5d3dde5ff8
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2025-04-13 01:37:58 +0900

    Add tooltip

--- web-app/src/app/provider.tsx
+++ web-app/src/app/provider.tsx
@@ -2,6 +2,7 @@
 
 import { MainErrorFallback } from '@/components/errors/main'
 import { Toaster } from '@/components/ui/sonner'
+import { TooltipProvider } from '@/components/ui/tooltip'
 import { env } from '@/config/env'
 import { ThemeProvider } from '@/providers/theme-provider'
 import { GoogleAnalytics } from '@next/third-parties/google'
@@ -17,7 +18,7 @@ export function AppProvider({ children }: { children: React.ReactNode }) {
 				enableSystem
 				disableTransitionOnChange
 			>
-				{children}
+				<TooltipProvider>{children}</TooltipProvider>
 			</ThemeProvider>
 			<Toaster position="top-center" />
 			<GoogleAnalytics gaId={env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID} />
--- web-app/src/features/images/components/image-cover.tsx
+++ web-app/src/features/images/components/image-cover.tsx
@@ -13,6 +13,11 @@ import {
 	PopoverContent,
 	PopoverTrigger,
 } from '@/components/ui/popover'
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipTrigger,
+} from '@/components/ui/tooltip'
 import { useKeepUntilOnPointerLeave } from '@/hooks/use-keep-until-on-pointer-leave'
 import { cn } from '@/utils/cn'
 import { PopoverClose } from '@radix-ui/react-popover'
@@ -56,63 +61,90 @@ export function ImageCover({
 		<article className={cn('relative', className)}>
 			<div className="group absolute inset-0 rounded-sm transition-all hover:bg-primary/50">
 				<div className="flex h-full items-center justify-center">
-					<Button
-						ref={copyButtonRef}
-						className="hidden cursor-pointer transition-all hover:bg-accent/50 active:bg-accent group-hover:inline-flex"
-						size="icon"
-						variant="ghost"
-						onClick={onClickCopyButton}
-					>
-						{isCopied ? (
-							<Check className="stroke-primary-foreground" />
-						) : (
-							<Clipboard className="stroke-primary-foreground" />
-						)}
-					</Button>
-					<Button
-						ref={downloadButtonRef}
-						className="hidden cursor-pointer transition-all hover:bg-accent/50 active:bg-accent group-hover:inline-flex"
-						size="icon"
-						variant="ghost"
-						onClick={onClickDownloadButton}
-					>
-						{isDownloaded ? (
-							<Check className="stroke-primary-foreground" />
-						) : (
-							<Download className="stroke-primary-foreground" />
-						)}
-					</Button>
-					<Dialog>
-						<DialogTrigger asChild>
+					<Tooltip delayDuration={700}>
+						<TooltipTrigger asChild>
 							<Button
+								ref={copyButtonRef}
 								className="hidden cursor-pointer transition-all hover:bg-accent/50 active:bg-accent group-hover:inline-flex"
 								size="icon"
 								variant="ghost"
+								onClick={onClickCopyButton}
 							>
-								<Trash className="stroke-primary-foreground" />
+								{isCopied ? (
+									<Check className="stroke-primary-foreground" />
+								) : (
+									<Clipboard className="stroke-primary-foreground" />
+								)}
 							</Button>
-						</DialogTrigger>
-						<DialogContent>
-							<DialogTitle>Delete Image</DialogTitle>
-							<DialogDescription>
-								Are you sure you want to delete this image?
-							</DialogDescription>
-							<DialogFooter>
-								<DialogClose asChild>
-									<Button className="cursor-pointer" variant="outline">
-										Cancel
+						</TooltipTrigger>
+						<TooltipContent>
+							<div className="flex items-center gap-2">
+								<p>Copy to clipboard</p>
+							</div>
+						</TooltipContent>
+					</Tooltip>
+					<Tooltip delayDuration={700}>
+						<TooltipTrigger asChild>
+							<Button
+								ref={downloadButtonRef}
+								className="hidden cursor-pointer transition-all hover:bg-accent/50 active:bg-accent group-hover:inline-flex"
+								size="icon"
+								variant="ghost"
+								onClick={onClickDownloadButton}
+							>
+								{isDownloaded ? (
+									<Check className="stroke-primary-foreground" />
+								) : (
+									<Download className="stroke-primary-foreground" />
+								)}
+							</Button>
+						</TooltipTrigger>
+						<TooltipContent>
+							<div className="flex items-center gap-2">
+								<p>Download</p>
+							</div>
+						</TooltipContent>
+					</Tooltip>
+					<Tooltip delayDuration={700}>
+						<Dialog>
+							<DialogTrigger asChild>
+								<TooltipTrigger asChild>
+									<Button
+										className="hidden cursor-pointer transition-all hover:bg-accent/50 active:bg-accent group-hover:inline-flex"
+										size="icon"
+										variant="ghost"
+									>
+										<Trash className="stroke-primary-foreground" />
 									</Button>
-								</DialogClose>
-								<Button
-									className="cursor-pointer"
-									variant="destructive"
-									onClick={onDelete}
-								>
-									Delete
-								</Button>
-							</DialogFooter>
-						</DialogContent>
-					</Dialog>
+								</TooltipTrigger>
+							</DialogTrigger>
+							<DialogContent>
+								<DialogTitle>Delete Image</DialogTitle>
+								<DialogDescription>
+									Are you sure you want to delete this image?
+								</DialogDescription>
+								<DialogFooter>
+									<DialogClose asChild>
+										<Button className="cursor-pointer" variant="outline">
+											Cancel
+										</Button>
+									</DialogClose>
+									<Button
+										className="cursor-pointer"
+										variant="destructive"
+										onClick={onDelete}
+									>
+										Delete
+									</Button>
+								</DialogFooter>
+							</DialogContent>
+						</Dialog>
+						<TooltipContent>
+							<div className="flex items-center gap-2">
+								<p>Delete</p>
+							</div>
+						</TooltipContent>
+					</Tooltip>
 				</div>
 			</div>
 			<button
