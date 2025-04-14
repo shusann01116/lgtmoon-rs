3260fd61df3619099b177e11d153eb33a994a46c
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2025-04-14 16:08:28 +0900

    Use neon client to take advantage of native support

--- web-app/src/lib/drizzle.ts
+++ web-app/src/lib/drizzle.ts
@@ -1,8 +1,4 @@
 import { env } from '@/config/env'
-import { drizzle } from 'drizzle-orm/postgres-js'
+import { drizzle } from 'drizzle-orm/neon-http'
 
-import postgres from 'postgres'
-
-const pool = postgres(env.POSTGRES_URL, { max: 1 })
-
-export const db = drizzle(pool)
+export const db = drizzle(env.POSTGRES_URL)
