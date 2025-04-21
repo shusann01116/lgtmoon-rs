291131b66e46dfe928687d513325a5dc75f2c17d
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2025-04-21 10:39:51 +0900

    Add local image migration

--- web-app/src/features/images/api/storage.ts
+++ web-app/src/features/images/api/storage.ts
@@ -1,13 +1,18 @@
 'use client'
 
 import type { LocalImage } from '@/types/lgtm-image'
-import { type DBSchema, type IDBPDatabase, openDB } from 'idb'
+import {
+	type DBSchema,
+	type IDBPDatabase,
+	type IDBPTransaction,
+	openDB,
+} from 'idb'
 import { useEffect, useRef, useState } from 'react'
 
 const dbConfig = {
 	name: 'lgtmoon',
 	storeName: 'lgtmoon',
-	version: 1,
+	version: 2,
 } as const
 
 export interface LgtMoonDb extends DBSchema {
@@ -46,8 +51,8 @@ export function useLgtMoonDb({
 				return
 			}
 			const db = await openDB<LgtMoonDb>(dbConfig.name, dbConfig.version, {
-				upgrade(db) {
-					db.createObjectStore(dbConfig.storeName, { keyPath: 'id' })
+				upgrade(db, oldVersion, newVersion, transaction) {
+					migrateDb(db, oldVersion, newVersion, transaction)
 				},
 			})
 			setDb(db)
@@ -58,4 +63,51 @@ export function useLgtMoonDb({
 	}, [onReady])
 
 	return db
+}
+
+function migrateDb(
+	db: IDBPDatabase<LgtMoonDb>,
+	oldVersion: number,
+	newVersion: number | null,
+	transaction: IDBPTransaction<
+		LgtMoonDb,
+		ArrayLike<'lgtmoon'>,
+		'versionchange'
+	>,
+) {
+	if (newVersion === null) {
+		return
+	}
+	for (let i = oldVersion; i < newVersion; i++) {
+		switch (i) {
+			case 0:
+				migrateDbV0ToV1(db)
+				break
+			case 1:
+				migrateDbV1ToV2(transaction)
+				break
+			default:
+				throw new Error('Failed to migrate db, Unknown version')
+		}
+	}
+}
+
+function migrateDbV0ToV1(db: IDBPDatabase<LgtMoonDb>) {
+	db.createObjectStore(dbConfig.storeName, { keyPath: 'id' })
+}
+
+async function migrateDbV1ToV2(
+	transaction: IDBPTransaction<
+		LgtMoonDb,
+		ArrayLike<'lgtmoon'>,
+		'versionchange'
+	>,
+) {
+	const store = transaction.objectStore('lgtmoon')
+	const images = await store.getAll()
+	for (const image of images) {
+		image.storage = 'local'
+		await store.put(image)
+	}
+	await transaction.done
 }
