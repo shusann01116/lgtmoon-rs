56cc703ec4b82c598bf4266fb6464aa7d288b487
shusann01116 <26602565+shusann01116@users.noreply.github.com>
2025-09-10 23:38:15 +0900

    Replace Geist with Noto Sans JP

--- web-app/src/app/layout.tsx
+++ web-app/src/app/layout.tsx
@@ -1,35 +1,28 @@
-import { AppProvider } from '@/app/provider'
-import '@/styles/globals.css'
-import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
+import { AppProvider } from "@/app/provider";
+import "@/styles/globals.css";
+import type { Metadata } from "next";
+import { Noto_Sans_JP } from "next/font/google";
 
-const geistSans = Geist({
-	variable: '--font-geist-sans',
-	subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-	variable: '--font-geist-mono',
-	subsets: ['latin'],
-})
+const notoSansJP = Noto_Sans_JP({
+	variable: "--font-noto-sans-jp",
+	subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
-	title: 'LGTMoon',
-	description: 'ファイルを選択したらLGTMを描いてくれるやつ made in Rust',
-}
+	title: "LGTMoon",
+	description: "ファイルを選択したらLGTMを描いてくれるやつ made in Rust",
+};
 
 export default function RootLayout({
 	children,
 }: Readonly<{
-	children: React.ReactNode
+	children: React.ReactNode;
 }>) {
 	return (
 		<html lang="ja">
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
+			<body className={`${notoSansJP.variable} antialiased`}>
 				<AppProvider>{children}</AppProvider>
 			</body>
 		</html>
-	)
+	);
 }
